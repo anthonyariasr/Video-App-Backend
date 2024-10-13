13fe1931118660419c2a4a0e6848bef1efb2d668
AnthonyAriass <anthonyarias2000@estudiantec.cr>
2024-10-12 19:03:39 -0600

    schema updated and css

--- app/schemas/videos_schema.py
+++ app/schemas/videos_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import datetime
 from typing import Optional
 
@@ -13,9 +13,17 @@ class VideoResponse(BaseModel):
     id: int
     title: str
     description: Optional[str]
+    creationDate: str  # Aquí mantenemos el campo como string
     videoPath: str
     thumbnailPath: Optional[str] = None
     isFavorite: bool
 
+    @validator('creationDate', pre=True, always=True)
+    def convert_creation_date(cls, value):
+        # Verificar si 'creationDate' es un datetime y convertirlo a string
+        if isinstance(value, datetime):
+            return value.strftime('%d-%m-%Y')
+        return value
+
     class Config:
-        orm_mode  = True
+        orm_mode = True
